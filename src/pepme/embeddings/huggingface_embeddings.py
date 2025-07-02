91f42040ad7fc72349e3e6abd8a90ff0202734f1
Jan Olszewski <janolszewski1906@gmail.com>
2025-07-02 16:27:34 +0200

    migrate esm to class

--- src/pepme/embeddings/huggingface_embeddings.py
+++ src/pepme/embeddings/huggingface_embeddings.py
@@ -1,4 +1,3 @@
-from functools import partial
 from typing import Literal, get_args
 
 import numpy as np
@@ -13,41 +12,47 @@ ESM2_Model_OPT = Literal[
 ]
 
 
-def compute_esm2_embeddings(
-    proteins: list[str],
-    device: str,
-    batch_size: int,
-    model_name: Literal[ESM2_Model_OPT],
-) -> np.ndarray:
-    tokenizer = AutoTokenizer.from_pretrained(f"facebook/{model_name}")
-    model = AutoModel.from_pretrained(f"facebook/{model_name}")
-    ret = []
-    with torch.inference_mode():
-        for i in RichProgress(
-            range(0, len(proteins), batch_size), "Computing ESM2 embeddings/BATCHES"
-        ):
-            batch = proteins[i : i + batch_size]
-            tokens = tokenizer(
-                batch, return_tensors="pt", padding=True, truncation=False
-            )
-            tokens = {
-                k: v.to(device) if isinstance(v, torch.Tensor) else v
-                for k, v in tokens.items()
-            }
-            hidden_state = model(**tokens).last_hidden_state
+class ESM2Embeddings:
+    def __init__(
+        self, model_name: Literal[ESM2_Model_OPT], device: str, batch_size: int
+    ):
+        self.tokenizer = AutoTokenizer.from_pretrained(f"facebook/{model_name}")
+        self.model = AutoModel.from_pretrained(f"facebook/{model_name}")
+        self.batch_size = batch_size
+        self.device = device
 
-            counts = tokens["attention_mask"].sum(dim=-1)
-            mask = tokens["attention_mask"]
-            for i, c in enumerate(counts):
-                mask[i, c - 1] = 0
-                mask[i, 0] = 0
-            counts = counts - 2
+    def __call__(
+        self,
+        proteins: list[str],
+    ) -> np.ndarray:
+        ret = []
+        with torch.inference_mode():
+            for i in RichProgress(
+                range(0, len(proteins), self.batch_size),
+                "Computing ESM2 embeddings/BATCHES",
+            ):
+                batch = proteins[i : i + self.batch_size]
+                tokens = self.tokenizer(
+                    batch, return_tensors="pt", padding=True, truncation=False
+                )
+                tokens = {
+                    k: v.to(self.device) if isinstance(v, torch.Tensor) else v
+                    for k, v in tokens.items()
+                }
+                hidden_state = self.model(**tokens).last_hidden_state
 
-            embed = (hidden_state * mask.unsqueeze(-1)).sum(dim=-2) / counts.unsqueeze(
-                -1
-            )
-            ret.append(embed.cpu().numpy())
-    return np.concatenate(ret)
+                counts = tokens["attention_mask"].sum(dim=-1)
+                mask = tokens["attention_mask"]
+                for i, c in enumerate(counts):
+                    mask[i, c - 1] = 0
+                    mask[i, 0] = 0
+                counts = counts - 2
+
+                embed = (hidden_state * mask.unsqueeze(-1)).sum(
+                    dim=-2
+                ) / counts.unsqueeze(-1)
+                ret.append(embed.cpu().numpy())
+        return np.concatenate(ret)
 
 
 HuggingFaceModel_OPT = Literal[ESM2_Model_OPT,]
@@ -66,11 +71,10 @@ def compute_huggingface_model_embeddings(
         batch_size = EngineCfg.BATCH_DIM()
 
     if opt in get_args(ESM2_Model_OPT):
-        encode_fn = partial(
-            compute_esm2_embeddings,
+        encode_fn = ESM2Embeddings(
+            model_name=opt,
             device=device,
             batch_size=batch_size,
-            model_name=opt,
         )
     else:
         raise NotImplementedError
