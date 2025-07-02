e8dba51c15d9c06d7e7215f89a5dae48d7c524a8
RasmusML <rasmusml@hotmail.dk>
2025-07-02 13:28:06 +0200

    Add tests for diversity metric

--- src/pepme/metrics/diversity.py
+++ src/pepme/metrics/diversity.py
@@ -1,47 +1,60 @@
-from typing import Optional, Literal
+from typing import Literal, Optional
 
 import numpy as np
-
-from pepme.core import Metric, MetricResult
 from Levenshtein import distance as lev
 
+from pepme.core import Metric, MetricResult
+
+
 class Diversity(Metric):
+    """
+    Diversity metric computes the fraction of sequences not in the reference set.
+    """
+
     def __init__(self, reference: list[str], reference_name: Optional[str] = None):
         """
-        Initialize the Novelty metric with a reference corpus.
+        Initialize the Diversity metric with a reference corpus.
 
         Args:
             reference (list[str]): A list of reference sequences against which
-                generated sequences will be compared. Sequences found in this
-                list are considered non-novel.
+                generated sequences will be compared.
             reference_name (Optional[str]): An optional label for the reference data.
                 This name will be appended to the metric name for identification.
                 Defaults to None.
         """
         self.reference = set(reference)
-        self.data_name = reference_name
+        self.reference_name = reference_name
 
-    @staticmethod
-    def _get_levenstein(seq_a: str, seq_b: str) -> int:
-        return lev(seq_a, seq_b)
-
-    def _get_levenshtein_for_all_ref_sequences(self, seq_a: str) -> list[int]:
-        return [self._get_levenstein(seq_a, seg_ref) for seg_ref in self.reference]
-
+        if len(self.reference) == 0:
+            raise ValueError("References must contain at least one sample.")
 
     def __call__(self, sequences: list[str]) -> MetricResult:
-        min_levenshtein_for_seqs = []
-        for sequence in sequences:
-            min_levenshtein_for_seqs.append(self._get_levenshtein_for_all_ref_sequences(sequence))
-        min_levenshtein_for_seqs_np = np.array(min_levenshtein_for_seqs)
-        return MetricResult(float(min_levenshtein_for_seqs_np.mean()), float(min_levenshtein_for_seqs_np.std()))
+        seqs_min_levenshtein = []
+        for seq in sequences:
+            min_distances = min(self._get_levenshtein_to_references(seq))
+            seqs_min_levenshtein.append(min_distances)
+
+        seqs_min_levenshtein_np = np.array(seqs_min_levenshtein)
+        return MetricResult(
+            seqs_min_levenshtein_np.mean().item(),
+            seqs_min_levenshtein_np.std().item(),
+        )
 
     @property
     def name(self) -> str:
-        return "Diversity" if self.data_name is None else f"Diversity ({self.data_name})"
+        return (
+            "Diversity"
+            if self.reference_name is None
+            else f"Diversity ({self.reference_name})"
+        )
 
     @property
     def objective(self) -> Literal["minimize", "maximize"]:
         return "maximize"
 
+    @staticmethod
+    def _get_levenshtein(sequence_a: str, sequence_b: str) -> int:
+        return lev(sequence_a, sequence_b)
 
+    def _get_levenshtein_to_references(self, sequence: str) -> list[int]:
+        return [self._get_levenshtein(sequence, ref) for ref in self.reference]
---
+++ tests/test_diversity.py
@@ -0,0 +1,46 @@
+import unittest
+
+import numpy as np
+
+from pepme.metrics.diversity import Diversity
+
+
+class TestDiversity(unittest.TestCase):
+    def test_compute_metric(self):
+        reference = ["AAA", "BBBB", "CCCCC"]
+        metric = Diversity(reference=reference, reference_name="Random")
+
+        # Name and objective properties
+        self.assertEqual(metric.name, "Diversity (Random)")
+        self.assertEqual(metric.objective, "maximize")
+
+        result = metric(["AA", "BB", "CCCCD"])
+
+        minimum_distances = np.array([1, 2, 1])
+
+        self.assertAlmostEqual(result.value, minimum_distances.mean())
+        self.assertAlmostEqual(result.deviation, minimum_distances.std())
+
+    def test_single_sequence(self):
+        reference = ["AAA", "BBBB", "CCCCC"]
+        metric = Diversity(reference=reference, reference_name="Random2")
+
+        # Name and objective properties
+        self.assertEqual(metric.name, "Diversity (Random2)")
+        self.assertEqual(metric.objective, "maximize")
+
+        result = metric(["AAA"])
+
+        self.assertAlmostEqual(result.value, 0.0)
+        self.assertAlmostEqual(result.deviation, 0.0)
+
+    def test_empty_reference(self):
+        with self.assertRaises(Exception) as context:
+            Diversity(reference=[])
+            self.assertTrue(
+                "References must contain at least one sample." in context.exception,
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
