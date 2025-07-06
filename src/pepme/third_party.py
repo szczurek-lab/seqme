57709140d4deab8854b50b38f50d71ff6f4765fc
RasmusML <rasmusml@hotmail.dk>
2025-07-06 23:42:22 +0200

    Add optional pyth executable path

--- src/pepme/third_party.py
+++ src/pepme/third_party.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any
+from typing import Any, Optional
 
 import numpy as np
 
@@ -22,6 +22,7 @@ class ThirdPartyModel:
         entry_point: str,
         repo_url: str,
         save_dir: str,
+        python_bin: Optional[str] = None,
     ):
         """
         Initialize and install the plugin.
@@ -31,6 +32,7 @@ class ThirdPartyModel:
                 in the form 'module.path:func_name'.
             repo_url (str): Git repository URL for the plugin (prefixed with 'git+' or local path).
             save_dir (str): Directory where the virtual environment and repo will be stored.
+            python_bin: Optional path to a python executable. If None, creates a venv enviroment using the exposed python executable.
 
         Raises:
             ValueError: If entry_point is not of the form 'module:func'.
@@ -44,13 +46,18 @@ class ThirdPartyModel:
         self.func = func
 
         self.plugin = Plugin()
-        self.plugin.setup(plugins_root=Path(save_dir), repo_url=repo_url)
+        self.plugin.setup(
+            plugins_root=Path(save_dir),
+            repo_url=repo_url,
+            python_bin=python_bin,
+        )
 
-    def __call__(self, **kwargs) -> np.ndarray:
+    def __call__(self, sequences: list[str], **kwargs) -> np.ndarray:
         """
         Execute the plugin's function with the given keyword arguments.
 
         Args:
+            sequences: List of sequences.
             **kwargs: Arbitrary keyword arguments to pass to the plugin function.
 
         Returns:
@@ -59,6 +66,7 @@ class ThirdPartyModel:
         Raises:
             ValueError: If the plugin response is not a numpy.ndarray.
         """
+        kwargs["sequences"] = sequences
         result = self.plugin.run(self.module, self.func, kwargs)
         if not isinstance(result, np.ndarray):
             raise ValueError("Invalid plugin response: expected numpy.ndarray")
@@ -70,25 +78,32 @@ class Plugin:
     Manages the environment and execution of a third-party plugin.
     """
 
-    def setup(self, plugins_root: Path, repo_url: str) -> None:
+    def setup(
+        self,
+        plugins_root: Path,
+        repo_url: str,
+        python_bin: Optional[str] = None,
+    ):
         """
         Create a virtual environment and install the plugin from its repository.
 
         Args:
             plugins_root (Path): Root directory for plugin environments and repos.
             repo_url (str): Git repository URL for the plugin.
+            python_bin: Optional path to a python executable. If None, creates a venv enviroment using the exposed python executable.
         """
         plugins_root.mkdir(parents=True, exist_ok=True)
-
-        env_dir = plugins_root / "env"
         repo_dir = plugins_root / "repo"
-        python_bin = env_dir / "bin" / "python"
 
-        if not env_dir.exists():
-            subprocess.check_call([sys.executable, "-m", "venv", str(env_dir)])
-            subprocess.check_call(
-                [str(python_bin), "-m", "pip", "install", "--upgrade", "pip"]
-            )
+        if python_bin is None:
+            env_dir = plugins_root / "env"
+            python_bin = env_dir / "bin" / "python"
+
+            if not env_dir.exists():
+                subprocess.check_call([sys.executable, "-m", "venv", str(env_dir)])
+                subprocess.check_call(
+                    [str(python_bin), "-m", "pip", "install", "--upgrade", "pip"]
+                )
 
         if not repo_dir.exists():
             url = repo_url.removeprefix("git+")
@@ -103,7 +118,7 @@ class Plugin:
         self,
         module: str,
         func: str,
-        input_data: dict,
+        arguments: dict,
     ) -> Any:
         """
         Run the specified function from the installed plugin.
@@ -111,7 +126,7 @@ class Plugin:
         Args:
             module (str): The module path containing the function to call.
             func (str): The name of the function within the module.
-            input_data (dict): Dictionary of arguments to serialize and pass.
+            arguments (dict): Dictionary of arguments passed to the function.
 
         Returns:
             Any: The deserialized output of the plugin's function.
@@ -122,7 +137,7 @@ class Plugin:
 
             # Serialize inputs
             with open(in_path, "wb") as f:
-                pickle.dump(input_data, f)
+                pickle.dump(arguments, f)
 
             # Construct and run inline Python execution
             code = (
