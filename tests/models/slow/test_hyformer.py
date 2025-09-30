1a44302a26d5948599b828d38653273882d608fd
AdamIzdebski <adam.izdebski1@gmail.com>
2025-09-30 17:07:53 +0200

    Refactor Hyformer test cases

--- tests/models/slow/test_hyformer.py
+++ tests/models/slow/test_hyformer.py
@@ -10,7 +10,7 @@ _EMBEDDING_DIM = 512
 
 @pytest.fixture(scope="module")
 def hyformer():
-    return Hyformer(model_name=HyformerCheckpoint.peptides_34M_mic, batch_size=32, device="cpu")
+    return Hyformer(model_name=HyformerCheckpoint.peptides_34M_mic, device="cpu")
 
 
 def test_hyformer_shape_and_means(hyformer):
@@ -30,18 +30,19 @@ def test_hyformer_shape_and_means(hyformer):
     # test perplexity
     perplexity = hyformer.compute_perplexity(data)
     assert perplexity.shape == (len(data),)
-    expected_perplexity = [3.03738141, 5.75629187]
+    expected_perplexity = np.array([3.03738117, 5.75628996])
     assert perplexity.tolist() == pytest.approx(expected_perplexity, abs=1e-6)
 
     # test generation
     generated_samples = hyformer.generate(num_samples=2, seed=1337)
     assert isinstance(generated_samples, list)
     assert isinstance(generated_samples[0], str)
-    expected_samples = ['WKKIWRRVV', 'RLQNIGKKVVKRLAKKLRRLKKK']
-    assert generated_samples == expected_samples
+    expected_samples = np.array(['KCKKWKWKKKLV', 'RWWRWWRWG'])
+    assert len(generated_samples) == len(expected_samples)
+    assert (generated_samples == expected_samples).all()
 
+    # test predictions
     predictions = hyformer.predict(data)
     assert predictions.shape == (len(data), 1)
-    assert isinstance(predictions[0], float)
-    expected_predictions = [0.46380734, 1.0693561]
+    expected_predictions = np.array([[0.46380645], [1.0693568]])
     assert predictions.tolist() == pytest.approx(expected_predictions, abs=1e-6)
